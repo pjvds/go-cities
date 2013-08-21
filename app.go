a264f0a7c5f795686ba3a40fb1061f134237ad6a
Pieter Joost van de Sande <pj@born2code.net>
2013-08-21 13:37:30 +0200

    Drop logging and add ping handler

--- app.go
+++ app.go
@@ -1,18 +1,19 @@
 package hello
 
 import (
-	"appengine"
 	"fmt"
 	"net/http"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/ping", pingHandler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	c.Infof("Requested URL: %v", r.URL)
-
 	fmt.Fprint(w, "Hello, world!")
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Pong!")
 }
