bc4febfa60da3a3b68748b90416c096978690c77
Pieter Joost van de Sande <pj@born2code.net>
2013-08-21 14:01:03 +0200

    Switch back to cities code

--- app.go
+++ app.go
@@ -1,20 +1,30 @@
 package hello
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+var (
+	cities = []string{
+		"Amsterdam", "San Francisco", "Paris", "New York", "Portland",
+	}
+)
+
 func init() {
 	r := http.NewServeMux()
-	r.HandleFunc("/", handler)
+	r.HandleFunc("/", handleIndex)
 	r.HandleFunc("/ping", pingHandler)
 
 	http.Handle("/", r)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+func handleIndex(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+
+	encoder := json.NewEncoder(rw)
+	encoder.Encode(cities)
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
